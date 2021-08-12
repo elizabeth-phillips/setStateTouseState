08d349d1ca69cecc57c7e39300a48da349f12c3c
elizabeth-phillips <elizabethcphillips63@gmail.com>
2021-08-12 15:05:04 -0500

    Converts to class component

--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
-//Could also be written: const Counter = (props) => {
-const Counter = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     count: props.initialCount
-  //   };
-  // }
-  const increment = () => setCount(count + 1);
-  // this.setState({
-  //   count: this.state.count + 1
-  // });
-  const decrement = () => setCount(count > 1 ? count - 1 : 0);
-  const reset = () => setCount(0);
+import React from 'react';
 
-  return (
-    <article>
-      <h2>Counter with setState</h2>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
-      <button onClick={reset}>0</button>
-      <p>The count is {count}</p>
-    </article>
-  );
-};
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: props.initialCount
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count > 1 ? this.state.count - 1 : 0
+    });
+  }
+  reset() {
+    this.setState({
+      count: 0
+    });
+  }
+
+  render() {
+    return (
+      <article>
+        <h2>Counter with setState</h2>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.reset}>0</button>
+        <p>The count is {this.state.count}</p>
+      </article>
+    );
+  }
+}
 
 export default Counter;
